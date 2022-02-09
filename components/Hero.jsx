b22bb5b5261b0f244a4635f3b4198b662aec5bf9
alekskimeu <kim3ual3x@gmail.com>
2022-02-09 12:13:02 +0300

    Set up the project

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+	return <div>Hero</div>;
+};
+
+export default Hero;
---
+++ components/Project.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Project = () => {
+	return <div>Project</div>;
+};
+
+export default Project;
