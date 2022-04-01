64fea9a743b7cf5b3878429b0ddd753ccd0905a2
alekskimeu <kim3ual3x@gmail.com>
2022-04-01 11:30:32 +0300

    Fix timeline line visibility

--- components/ResumeItem/ResumeItem.jsx
+++ components/ResumeItem/ResumeItem.jsx
@@ -1,25 +1,32 @@
-import styles from "./ResumeItem.module.css";
+import { useTheme } from 'next-themes';
+
+import styles from './ResumeItem.module.css';
 
 const ResumeItem = ({ title, company, timeline, description, duties }) => {
-	return (
-		<div className={styles.resumeItem}>
-			<div className={styles.resumeItemHeader}>
-				<div>
-					<h1 className={styles.profession}>{title}</h1>
-					<h2 className={styles.company}>{company}</h2>
-				</div>
-				<p className={styles.timeline}>{timeline}</p>
-			</div>
-			<div className={styles.resumeBody}>
-				<p className={styles.description}>{description}</p>
-				<ul className={styles.resumeList}>
-					{duties.map((duty) => (
-						<li key={duty.index}>{duty}</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div
+      className={styles.resumeItem}
+      style={{ border: theme === 'light' ? '2px solid lightgray' : 'none' }}
+    >
+      <div className={styles.resumeItemHeader}>
+        <div>
+          <h1 className={styles.profession}>{title}</h1>
+          <h2 className={styles.company}>{company}</h2>
+        </div>
+        <p className={styles.timeline}>{timeline}</p>
+      </div>
+      <div className={styles.resumeBody}>
+        <p className={styles.description}>{description}</p>
+        <ul className={styles.resumeList}>
+          {duties.map(duty => (
+            <li key={duty.index}>{duty}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeItem;
