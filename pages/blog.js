3fd8c3ff67e8c36e5122c0adab2196f2fa821b0e
alekskimeu <kim3ual3x@gmail.com>
2022-04-07 19:08:02 +0300

    Add search functionality for posts on Blog page

--- pages/blog.js
+++ pages/blog.js
@@ -3,53 +3,61 @@ import Head from "next/head";
 
 
 import { useTheme } from "next-themes";
+import { useState } from 'react';
 
 
 
-import { Post } from "../components";
-import { getSortedPostsData } from "../lib/posts";
-import styles from "../styles/Blog.module.css";
+import { Post } from '../components';
+import { getSortedPostsData } from '../lib/posts';
+import styles from '../styles/Blog.module.css';
+
 
 
 // TODO: Add Postman to the list of tools I use
 
 export default function Blog({ posts }) {
-	const { theme, setTheme } = useTheme();
-	return (
-		<>
-			<Head>
-				<title>Alex Kimeu - Blog</title>
-				<meta name="description" content="Writing on technical topics and techs I learn" />
-				<meta name="keywords" content="Blog, Technical writing" />
-			</Head>
-			<main className={styles.blog}>
-				<div className="container">
-					<div
-						className={styles.search}
-						style={{
-							background: theme === "light" ? "rgb(231, 231, 231)" : "white",
-						}}
-					>
-						<i className="fas fa-search"></i>
-						<input
-							type="search"
-							name="query"
-							placeholder="Search Article"
-							className={styles.searchInput}
-							style={{
-								background: theme === "light" ? "rgb(231, 231, 231)" : "white",
-							}}
-						/>
-					</div>
-					<div className={styles.postsWrapper}>
-						{posts.map((post) => (
-							<Post key={post.index} post={post} />
-						))}
-					</div>
-				</div>
-			</main>
-		</>
-	);
+  const { theme, setTheme } = useTheme();
+  const [query, setQuery] = useState('');
+  return (
+    <>
+      <Head>
+        <title>Alex Kimeu - Blog</title>
+        <meta
+          name="description"
+          content="Writing on technical topics and techs I learn"
+        />
+        <meta name="keywords" content="Blog, Technical writing" />
+      </Head>
+      <main className={styles.blog}>
+        <div className="container">
+          <div
+            className={styles.search}
+            style={{
+              background: theme === 'light' ? 'rgb(231, 231, 231)' : 'white'
+            }}
+          >
+            <i className="fas fa-search"></i>
+            <input
+              type="search"
+              name="query"
+              placeholder="Search Article"
+						  className={styles.searchInput}
+						  value={query}
+						  onChange={(e) => setQuery(e.target.value)}
+              style={{
+                background: theme === 'light' ? 'rgb(231, 231, 231)' : 'white'
+              }}
+            />
+          </div>
+          <div className={styles.postsWrapper}>
+            {posts.filter(post => post.content.includes(query) || post.title.includes(query)).map(post => (
+              <Post key={post.index} post={post} />
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export async function getStaticProps() {
