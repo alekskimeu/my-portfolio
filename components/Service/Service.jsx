06aa1101dc919a48a53c48e019a4a45dd7e84457
alekskimeu <kim3ual3x@gmail.com>
2022-04-08 14:00:19 +0300

    Make service details visible on light theme

--- components/Service/Service.jsx
+++ components/Service/Service.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import { useTheme } from "next-themes";
 import styles from "./Service.module.css";
 
 const Service = ({ service }) => {
+	const { theme, setTheme } = useTheme();
+
 	return (
 		<div className={styles.service}>
 			{service.icon}
 			<div className={styles.content}>
-				<h1 className={styles.title}>{service.title}</h1>
-				<p className={styles.description}>{service.description}</p>
+				<h1
+					className={styles.title}
+					style={{ color: theme === "light" ? "#072227" : "white" }}
+				>
+					{service.title}
+				</h1>
+				<p
+					className={styles.description}
+					style={{ color: theme === "light" ? "#072227" : "white" }}
+				>
+					{service.description}
+				</p>
 			</div>
 		</div>
 	);
