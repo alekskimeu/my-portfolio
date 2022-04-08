33100e5cd3c8cca9cc0dc5c393a4e0d788341ae3
alekskimeu <kim3ual3x@gmail.com>
2022-04-08 08:53:01 +0300

    Add default export statements for the posts & projects components

--- components/index.js
+++ components/index.js
@@ -6,7 +6,9 @@ export { default as HeroImage } from "./HeroImage/HeroImage";
 export { default as Layout } from "./Layout/Layout";
 export { default as NoItems } from './NoItems/NoItems';
 export { default as Post } from "./Post/Post";
+export { default as Posts } from './Posts/Posts';
 export { default as Project } from "./Project/Project";
+export { default as Projects } from './Projects/Projects';
 export { default as ResumeItem } from "./ResumeItem/ResumeItem";
 export { default as Skills } from "./Skills/Skills";
 
