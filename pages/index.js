e75ca619d030d7a1ce69acfef3201cf921228078
alekskimeu <kim3ual3x@gmail.com>
2022-04-08 08:51:50 +0300

    Cleaned up the index page: Moved projects & posts to their own components

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,40 @@
 import Head from "next/head";
 
-import { Hero, Post, Project, Skills } from "../components";
-import { getSortedPostsData } from "../lib/posts";
-import { projectsData } from "../data/projects/data";
-import styles from "../styles/Home.module.css";
+
+
+import { Hero, Posts, Projects, Skills } from '../components';
+import { getSortedPostsData } from '../lib/posts';
 
 export default function Home({ posts }) {
-	const projects = projectsData.slice(0, 2);
-	return (
-		<>
-			<Head>
-				<title>Alex Kimeu - Homepage </title>
-				<meta
-					name="description"
-					content="Software Developer | Limitless learner | Blogger"
-				/>
-				<meta name="keywords" content="Alex Kimeu, Software Developer" />
-			</Head>
-			<main>
-				<Hero />
-				<Skills />
+  return (
+    <>
+      <Head>
+        <title>Alex Kimeu - Homepage </title>
+        <meta
+          name="description"
+          content="Software Developer | Limitless learner | Blogger"
+        />
+        <meta name="keywords" content="Alex Kimeu, Software Developer" />
+      </Head>
+      <main>
+        <Hero />
+        <Skills />
 
-				{/* Projects */}
-				<div className={styles.projects}>
-					<div className="container">
-						<div className="section-header">
-							<h1 className="heading">Projects</h1>
-						</div>
-						<div className={styles.projectsWrapper}>
-							{projects.map((project) => (
-								<Project key={project.index} project={project} />
-							))}
-						</div>
-					</div>
-				</div>
+        {/* Projects */}
+        <Projects />
 
-				{/* Posts */}
-				<div className={styles.blogPosts}>
-					<div className="container">
-						<div className="section-header">
-							<h1 className="heading">Latest posts</h1>
-						</div>
-						<div className={styles.posts}>
-							{posts.map((post) => (
-								<Post key={post.index} post={post} />
-							))}
-						</div>
-					</div>
-				</div>
-			</main>
-		</>
-	);
+        {/* Posts */}
+        <Posts posts={posts} />
+      </main>
+    </>
+  );
 }
 
 // Fetch posts & projects
 export async function getStaticProps() {
-	const posts = getSortedPostsData().slice(0, 3);
+  const posts = getSortedPostsData().slice(0, 3);
 
-	return {
-		props: { posts },
-	};
+  return {
+    props: { posts }
+  };
 }
