8b8d3cc3443a33a5f10308ee0fb1aedc323c233a
alekskimeu <kim3ual3x@gmail.com>
2022-04-08 16:39:05 +0300

    Update project data: Soko store

--- data/projects/data.js
+++ data/projects/data.js
@@ -1,30 +1,30 @@
 export const projectsData = [
-  {
-    title: 'Portfolio',
-    description: 'My portfolio website',
-    techs: ['React', 'Next JS', 'Markdown'],
-    code: 'https://github.com/alekskimeu/my-portfolio',
-    demo: 'https://alex-kimeu.com'
-  },
-  {
-    title: 'Soko store',
-    description: 'eCommerce frontend web platform.',
-    techs: ['React', 'Redux', 'CSS3'],
-    code: 'https://github.com/alekskimeu/soko-store',
-    demo: 'https://boring-mccarthy-46c8cc.netlify.app/'
-  },
-  {
-    title: 'BDADG Website',
-    description: 'Website for BDADG team',
-    techs: ['React', 'Next JS', 'Markdown'],
-    code: 'https://github.com/alekskimeu/bdadg-website',
-    demo: 'https://bdadg-website.vercel.app/'
-  },
-  {
-    title: 'Twitter Bot',
-    description: 'Likes, retweets tech content & follows back.',
-    techs: ['Python', 'Tweepy'],
-    code: 'https://github.com/alekskimeu/twitter-bot',
-    demo: 'https://twitter.com/lexbot0'
-  }
+	{
+		title: "Portfolio",
+		description: "My portfolio website",
+		techs: ["React", "Next JS", "Markdown"],
+		code: "https://github.com/alekskimeu/my-portfolio",
+		demo: "https://alex-kimeu.com",
+	},
+	{
+		title: "Soko store",
+		description: "eCommerce frontend site.",
+		techs: ["React", "Redux", "CSS3"],
+		code: "https://github.com/alekskimeu/soko-store",
+		demo: "https://boring-mccarthy-46c8cc.netlify.app/",
+	},
+	{
+		title: "BDADG Website",
+		description: "Website for BDADG team",
+		techs: ["React", "Next JS", "Markdown"],
+		code: "https://github.com/alekskimeu/bdadg-website",
+		demo: "https://bdadg-website.vercel.app/",
+	},
+	{
+		title: "Twitter Bot",
+		description: "Likes, retweets tech content & follows back.",
+		techs: ["Python", "Tweepy"],
+		code: "https://github.com/alekskimeu/twitter-bot",
+		demo: "https://twitter.com/lexbot0",
+	},
 ];
