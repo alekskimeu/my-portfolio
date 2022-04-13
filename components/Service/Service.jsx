d4c33ff258fa7ebb47051f09d296693f9a9f6270
alekskimeu <kim3ual3x@gmail.com>
2022-04-13 21:18:06 +0300

    Change content colors

--- components/Service/Service.jsx
+++ components/Service/Service.jsx
@@ -11,10 +11,19 @@ const Service = ({ service }) => {
 				boxShadow:
 					theme === "light"
 						? "5px 8px 16px rgb(231, 231, 231)"
-						: "5px 8px 16px #141E27",
+						: "5px 8px 16px #000",
+				borderTop:
+					theme === "light"
+						? "3px solid #072227"
+						: "3px solid rgba(231, 231, 231)",
 			}}
 		>
-			<div className={styles.icon}>{service.icon}</div>
+			<div
+				className={styles.icon}
+				style={{ color: theme === "light" ? "#072227" : "white" }}
+			>
+				{service.icon}
+			</div>
 			<div className={styles.content}>
 				<h1 className={styles.title}>{service.title}</h1>
 				<p
