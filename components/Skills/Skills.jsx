fb36d627276ce19629f9337f0bc2daa46bfde1ac
alekskimeu <kim3ual3x@gmail.com>
2022-04-20 21:01:24 +0300

    Changed skills order

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -92,40 +92,6 @@ const Skills = () => {
 							</div>
 						</div>
 					</div>
-					<div>
-						{" "}
-						<div
-							className={styles.skillsCard}
-							data-aos="fade-up"
-							data-aos-duration="1000"
-							style={{
-								borderTop:
-									theme === "light" ? "2px solid #072227" : "1px solid white",
-								boxShadow:
-									theme === "light"
-										? "5px 8px 16px rgb(231, 231, 231)"
-										: "5px 8px 16px #072227",
-							}}
-						>
-							<div className={styles.skillsHeader}>
-								<h1 className={styles.heading}>Database</h1>
-							</div>
-							<div className={styles.skillsContainer}>
-								<Image
-									src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
-									alt="MySQL"
-									width={50}
-									height={50}
-								/>
-								<Image
-									src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
-									alt="MongoDB"
-									width={50}
-									height={50}
-								/>
-							</div>
-						</div>
-					</div>
 
 					<div>
 						{" "}
@@ -155,6 +121,41 @@ const Skills = () => {
 								<Image
 									src="https://raw.githubusercontent.com/github/explore/5b3600551e122a3277c2c5368af2ad5725ffa9a1/topics/java/java.png"
 									alt="Java"
+									width={50}
+									height={50}
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div>
+						{" "}
+						<div
+							className={styles.skillsCard}
+							data-aos="fade-up"
+							data-aos-duration="1000"
+							style={{
+								borderTop:
+									theme === "light" ? "2px solid #072227" : "1px solid white",
+								boxShadow:
+									theme === "light"
+										? "5px 8px 16px rgb(231, 231, 231)"
+										: "5px 8px 16px #072227",
+							}}
+						>
+							<div className={styles.skillsHeader}>
+								<h1 className={styles.heading}>Database</h1>
+							</div>
+							<div className={styles.skillsContainer}>
+								<Image
+									src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+									alt="MySQL"
+									width={50}
+									height={50}
+								/>
+								<Image
+									src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+									alt="MongoDB"
 									width={50}
 									height={50}
 								/>
