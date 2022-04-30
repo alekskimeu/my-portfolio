e25f19f998fb0c071319c9803f3b473e074c76f6
alekskimeu <kim3ual3x@gmail.com>
2022-04-30 11:23:55 +0300

    Change nav links order

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -19,11 +19,11 @@ const Header = () => {
 					<Link href="/about">
 						<a>About</a>
 					</Link>
-					<Link href="/resume">
-						<a>Resume</a>
-					</Link>
 					<Link href="/blog">
 						<a>Blog</a>
+					</Link>
+					<Link href="/resume">
+						<a>Resume</a>
 					</Link>
 				</div>
 			</nav>
