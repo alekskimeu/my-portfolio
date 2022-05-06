cacff37d9a73887a056fa376a4e2eeee5af9b7e0
alekskimeu <kim3ual3x@gmail.com>
2022-05-06 14:56:47 +0300

    Modify Hero & FaturedPOsts components

--- components/FeaturedPosts/FeaturedPosts.jsx
+++ components/FeaturedPosts/FeaturedPosts.jsx
@@ -6,9 +6,6 @@ const FeaturedPosts = ({ featured }) => {
 	return (
 		<div className={styles.blogPosts}>
 			<div className={styles.container}>
-				<div className="section-header">
-					<h1 className="heading">Featured posts</h1>
-				</div>
 				<div className={styles.featuredPosts}>
 					{featured.map((post) => (
 						<Post key={post.index} post={post} />
