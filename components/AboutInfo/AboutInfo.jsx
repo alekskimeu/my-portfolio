231743df8f727a9043ae62646e9a28a42bd45660
alekskimeu <kim3ual3x@gmail.com>
2022-05-11 14:33:47 +0300

    Add KoTH link in About section

--- components/AboutInfo/AboutInfo.jsx
+++ components/AboutInfo/AboutInfo.jsx
@@ -5,25 +5,41 @@ const AboutInfo = () => {
 		<div className={styles.aboutInfo}>
 			<p className={styles.description}>
 				Hello, my name is Alex Kimeu and I&#39;m a Software Developer based in
-				Nairobi, Kenya.
+				Nairobi, Kenya. I have hands-on experience in building mobile & web
+				applications using modern technologies.
 			</p>
 			<p className={styles.description}>
 				Apart from coding & learning new techs, I write about tech in my{" "}
 				<Link href="/blog">
-					<a>Blog</a>
-				</Link>
-				, & occasionally play King of The Hill on TryHackMe.
+					<a className={styles.link}>Blog</a>
+				</Link>{" "}
+				& occasionally play{" "}
+				<a
+					href="https://tryhackme.com/games/koth"
+					target="_blank"
+					rel="noreferrer"
+					className={styles.link}
+				>
+					KoTH
+				</a>{" "}
+				on TryHackMe.
 			</p>
 			<p className={styles.description}>
-				I&#39;m available for freelance projects. Shoot me an{" "}
-				<a href="mailto:kimeualeks@gmail.com" target="_blank" rel="noreferrer">
+				If you would like to hire me for freelance projects, please shoot me an{" "}
+				<a
+					href="mailto:kimeualeks@gmail.com"
+					target="_blank"
+					rel="noreferrer"
+					className={styles.link}
+				>
 					Email
 				</a>{" "}
-				& let&#39;s work together.
+				& let&#39;s build awesome stuff.
 			</p>
 			<p className={styles.description}>
 				You can find my work on{" "}
 				<a
+					className={styles.link}
 					href="https://github.com/alekskimeu"
 					target="_blank"
 					rel="noreferrer"
