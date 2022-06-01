908d85fd0bf2dbb16442963e329ab1732c3467e9
alekskimeu <kim3ual3x@gmail.com>
2022-06-01 10:25:44 +0300

    Added CV download link

--- components/AppButton/AppButton.js
+++ components/AppButton/AppButton.js
@@ -12,7 +12,7 @@ const AppButton = ({ text }) => {
 
 	return (
 		<a
-			href=""
+			href="https://drive.google.com/uc?export=download&id=1an5BM4BGBz2jUWSc-gu9p2mypI_Hp3Ug"
 			className={styles.btn}
 			data-aos="zoom-in"
 			data-aos-duration="1000"
