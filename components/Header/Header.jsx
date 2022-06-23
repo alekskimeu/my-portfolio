dc3a94cc9919126360711def3868edd63a630302
alekskimeu <kim3ual3x@gmail.com>
2022-06-23 15:01:39 +0300

    Modify pages styles

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -6,27 +6,16 @@ import Image from "next/image";
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<nav className={styles.navbar}>
-				{/* <Link href="/">
-					<a className={styles.logo}>
-						<Image src="/favicon-32x32.png" height={32} width={32} alt="Logo" />
-					</a>
-				</Link> */}
-				<div className={styles.menu}>
-					<Link href="/">
-						<a>Home</a>
-					</Link>
-					<Link href="/about">
-						<a>About</a>
-					</Link>
-					<a
-						href="https://aleks.hashnode.dev/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Blog
-					</a>
-				</div>
+			<nav className={styles.menu}>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/about">
+					<a>About</a>
+				</Link>
+				<a href="https://aleks.hashnode.dev/" target="_blank" rel="noreferrer">
+					Blog
+				</a>
 			</nav>
 		</header>
 	);
